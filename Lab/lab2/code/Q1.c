4cf41f85fe100a541621ea62dbd2298080a3d678
yitong <yitongcool@foxmail.com>
2022-02-25 15:52:19 +0800

    Update Lab2

---
+++ Lab/lab2/code/Q1.c
@@ -0,0 +1,6 @@
+#include <math.h>
+#include <stdio.h>
+int main(){
+	printf("%lf\n",sqrt(2.0));
+	return 0;
+}
---
+++ Lab/lab2/code/Q2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("%s %d\n","Yitong WANG", 11910104);
+	return 0;
+}
